838d427fedea8ccfa8aad94c397c944309072f1e
MohdSajid12 <108975916+MohdSajid12@users.noreply.github.com>
2022-07-25 21:33:13 +0530

    Update Booking.js

--- Booking Appointment App/Booking.js
+++ Booking Appointment App/Booking.js
@@ -19,6 +19,15 @@
     })
     // showNewUserOnScreen(obj)
   }
+axios.post("https://crudcrud.com/api/ed3b65b9bdb0404496477ac50a01834f/AppointmentData",obj)
+    .then((response) =>{
+      showNewUserOnScreen(response.data)
+      console.log(response)
+    }) 
+    .catch((err)=>{
+
+      console.log(err)
+    })
   function showNewUserOnScreen(user){
     const parentNode = document.getElementById('items');
       const childHTML = `<li id =${user.email}> ${user.name} - ${user.email} <button onclick=deleteUser('${user.email}')> Delete Data 
